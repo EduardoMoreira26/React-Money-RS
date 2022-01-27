a21248133270cc978158212981515f5011dbf63e
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-27 17:59:33 -0300

    create summary and dashboard archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "../src/components/Header";
+import { Dashboard } from "../src/components/Dashboard";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard/>
       <GlobalStyle />
     </>
   );
