f3bea1112d8cbc22ff2dd0c0bd4596894453f821
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-27 21:33:58 -0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "../src/components/Header";
 import { Dashboard } from "../src/components/Dashboard";
+import Modal from 'react-modal';
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionMoral={handleOpenNewTransactionModal} />
       <Dashboard/>
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+       
+      <h1>Modal</h1>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
-import Modal from 'react-modal';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionMoral: () => void;
+}
 
-const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false);
-  }
-
+export function Header({ onOpenNewTransactionMoral }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="at money" />
-        <button onClick={handleOpenNewTransactionModal} type="button">Nova transação</button>
+        <button onClick={onOpenNewTransactionMoral} type="button">
+          Nova transação
+        </button>
       </Content>
-
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-       
-      <h1>Modal</h1>
-      </Modal>
     </Container>
   );
 }
